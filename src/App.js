5073f9357fe1536ea993bd6fc4869fe3678a85a0
Yulia Ratseburzhynska <bachkovska@gmail.com>
2022-07-26 11:21:21 +0300

    search engine is done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import Dictionary from "./Dictionary";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>
+        DISCOVER THE WORLD <br /> through the meanings of words
+      </h1>
+      <Dictionary />
+      <footer className="text-center">Coded by Yulia Ratseburzhynska</footer>
+    </div>
+  );
+}
